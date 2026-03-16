ca154c04051b27d57b8138fae63916155fce0526
ashik-zainik-lab <ashik.200607.zan@gmail.com>
2026-03-16 15:52:45 +0600

    working on application and logo settings

--- resources/views/admin/setting/general_settings/application-settings.blade.php
+++ resources/views/admin/setting/general_settings/application-settings.blade.php
@@ -4,129 +4,123 @@
 @endpush
 @section('content')
     <div class="p-30">
-        <div class="">
-            <h4 class="fs-24 fw-500 lh-34 text-black pb-16">{{ __($title) }}</h4>
-            <div class="row">
-                <div class="col-xxl-2 col-lg-3 col-md-4 pr-0">
-                    <div class="bd-c-ebedf0 bd-half bd-ra-25 bg-white h-100 p-30">
-                        @include('admin.setting.partials.general-sidebar')
-                    </div>
-                </div>
-                <div class="col-xxl-10 col-lg-9 col-md-8">
-                    <div class="bg-white bd-half bd-c-ebedf0 bd-ra-25 p-30">
-                        <div class="email-inbox__area bg-style form-horizontal__item bg-style admin-general-settings-page">
-                            <div class="item-top mb-30">
-                                <h4>{{ $title }}</h4>
-                            </div>
-                            <form class="ajax" action="{{ route('admin.setting.application-settings.update') }}"
-                                method="POST" enctype="multipart/form-data" data-handler="settingCommonHandler">
-                                @csrf
-                                <div class="row">
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <div class="primary-form-group my-2 pt-3">
-                                            <div class="primary-form-group-wrap">
-                                                <label class="form-label">{{ __('App Name') }} <span
-                                                        class="text-danger">*</span></label>
-                                                <div class="">
-                                                    <input type="text" name="app_name"
-                                                        value="{{ getOption('app_name') }}" class="primary-form-control">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    @if(!isAddonInstalled('ALUSAAS'))
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <div class="primary-form-group my-2 pt-3">
-                                            <div class="primary-form-group-wrap">
-                                                <label class="form-label">{{ __('App URL') }} <span
-                                                        class="text-danger">*</span></label>
-                                                <div class="">
-                                                    <input type="text" name="APP_URL"
-                                                           value="{{ getOption('APP_URL') }}" class="primary-form-control">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    @endif
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <div class="primary-form-group my-2 pt-3">
-                                            <div class="primary-form-group-wrap">
-                                                <label class="form-label">{{ __('App Email') }} <span
-                                                        class="text-danger">*</span></label>
-                                                <input type="text" name="app_email" value="{{ getOption('app_email') }}"
-                                                    class="primary-form-control">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <div class="primary-form-group my-2 pt-3">
-                                            <div class="primary-form-group-wrap">
-                                                <label class="form-label">{{ __('App Contact Number') }} <span
-                                                        class="text-danger">*</span></label>
-                                                <input type="text" name="app_contact_number"
-                                                    value="{{ getOption('app_contact_number') }}"
-                                                    class="primary-form-control">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <div class="primary-form-group my-2 pt-3">
-                                            <div class="primary-form-group-wrap">
-                                                <label class="form-label">{{ __('App Location') }} <span
-                                                        class="text-danger">*</span></label>
-                                                <input type="text" name="app_location"
-                                                    value="{{ getOption('app_location') }}" class="primary-form-control">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    @if(!isAddonInstalled('ALUSAAS'))
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <div class="primary-form-group my-2 pt-3">
-                                            <div class="primary-form-group-wrap">
-                                                <label class="form-label">{{ __('App Copyright') }} <span
-                                                        class="text-danger">*</span></label>
-                                                <input type="text" name="app_copyright"
-                                                    value="{{ getOption('app_copyright') }}" class="primary-form-control">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <div class="primary-form-group my-2 pt-3">
-                                            <div class="primary-form-group-wrap">
-                                                <label class="form-label">{{ __('Developed By') }} <span
-                                                        class="text-danger">*</span></label>
-                                                <input type="text" name="app_developed"
-                                                    value="{{ getOption('app_developed') }}" class="primary-form-control">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    @endif
-                                    <div class="col-xxl-4 col-lg-6">
-                                        <div class="primary-form-group my-2 pt-3">
-                                            <div class="primary-form-group-wrap">
-                                                <label for="app_timezone" class="form-label">{{ __('Timezone') }} <span
-                                                        class="text-danger">*</span></label>
-                                                <select name="app_timezone" class="form-control sf-select">
-                                                    @foreach ($timezones as $timezone)
-                                                        <option value="{{ $timezone }}"
-                                                            {{ $timezone == getOption('app_timezone') ? 'selected' : '' }}>
-                                                            {{ $timezone }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-12">
-                                            <div class="input__group general-settings-btn text-end">
-                                                <button type="submit"
-                                                    class="bd-ra-12 bg-cdef84 border-0 fw-500 hover-bg-one lh-25 px-26 py-10">{{ __('Update') }}</button>
-                                            </div>
-                                        </div>
+        <!-- Page title like setting.html -->
+        <div class="dashboard-section-title">
+            <h2 class="title">{{ __($title) }}</h2>
+            <p>{{ __('Manage core application information and contact details.') }}</p>
+        </div>
+
+        <!-- Settings layout like html/setting.html -->
+        <div class="d-flex align-items-start settings-grid-container">
+            <!-- Left sidebar tabs (uses updated general-sidebar) -->
+            <aside class="settings-sidebar">
+                @include('admin.setting.partials.general-sidebar')
+            </aside>
+
+            <!-- Right content -->
+            <div class="settings-forms w-100">
+                <div class="dashboard-settings-card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ __('Application Settings') }}</h5>
+
+                        <form class="ajax" action="{{ route('admin.setting.application-settings.update') }}"
+                              method="POST" enctype="multipart/form-data" data-handler="settingCommonHandler">
+                            @csrf
+                            <div class="row g-3">
+                                <div class="col-xxl-4 col-lg-6 col-md-6">
+                                    <div class="dashboard-form-group">
+                                        <label class="form-label">{{ __('App Name') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" name="app_name"
+                                               value="{{ getOption('app_name') }}" class="primary-form-control">
                                     </div>
                                 </div>
-                            </form>
-                        </div>
+
+                                @if(!isAddonInstalled('ALUSAAS'))
+                                    <div class="col-xxl-4 col-lg-6 col-md-6">
+                                        <div class="dashboard-form-group">
+                                            <label class="form-label">{{ __('App URL') }} <span
+                                                    class="text-danger">*</span></label>
+                                            <input type="text" name="APP_URL"
+                                                   value="{{ getOption('APP_URL') }}" class="primary-form-control">
+                                        </div>
+                                    </div>
+                                @endif
+
+                                <div class="col-xxl-4 col-lg-6 col-md-6">
+                                    <div class="dashboard-form-group">
+                                        <label class="form-label">{{ __('App Email') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" name="app_email" value="{{ getOption('app_email') }}"
+                                               class="primary-form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-xxl-4 col-lg-6 col-md-6">
+                                    <div class="dashboard-form-group">
+                                        <label class="form-label">{{ __('App Contact Number') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" name="app_contact_number"
+                                               value="{{ getOption('app_contact_number') }}"
+                                               class="primary-form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-xxl-4 col-lg-6 col-md-6">
+                                    <div class="dashboard-form-group">
+                                        <label class="form-label">{{ __('App Location') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" name="app_location"
+                                               value="{{ getOption('app_location') }}" class="primary-form-control">
+                                    </div>
+                                </div>
+
+                                @if(!isAddonInstalled('ALUSAAS'))
+                                    <div class="col-xxl-4 col-lg-6 col-md-6">
+                                        <div class="dashboard-form-group">
+                                            <label class="form-label">{{ __('App Copyright') }} <span
+                                                    class="text-danger">*</span></label>
+                                            <input type="text" name="app_copyright"
+                                                   value="{{ getOption('app_copyright') }}" class="primary-form-control">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xxl-4 col-lg-6 col-md-6">
+                                        <div class="dashboard-form-group">
+                                            <label class="form-label">{{ __('Developed By') }} <span
+                                                    class="text-danger">*</span></label>
+                                            <input type="text" name="app_developed"
+                                                   value="{{ getOption('app_developed') }}" class="primary-form-control">
+                                        </div>
+                                    </div>
+                                @endif
+
+                                <div class="col-xxl-4 col-lg-6 col-md-6">
+                                    <div class="dashboard-form-group">
+                                        <label for="app_timezone" class="form-label">{{ __('Timezone') }} <span
+                                                class="text-danger">*</span></label>
+                                        <select name="app_timezone" class="form-control sf-select">
+                                            @foreach ($timezones as $timezone)
+                                                <option value="{{ $timezone }}"
+                                                    {{ $timezone == getOption('app_timezone') ? 'selected' : '' }}>
+                                                    {{ $timezone }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row mt-3">
+                                <div class="col-12">
+                                    <div class="input__group general-settings-btn text-end">
+                                        <button type="submit"
+                                                class="bd-ra-12 bg-cdef84 border-0 fw-500 hover-bg-one lh-25 px-26 py-10">
+                                            {{ __('Update') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- resources/views/admin/setting/partials/general-sidebar.blade.php
+++ resources/views/admin/setting/partials/general-sidebar.blade.php
@@ -1,48 +1,31 @@
-<div class="email__sidebar bg-style">
-    <div class="sidebar__item">
-        <ul class="d-flex flex-column rg-15 sidebar__mail__nav">
-            <li>
-                <a href="{{ route('admin.setting.application-settings') }}"
-                   class="align-items-center flex list-item list-item">
-                    <span class="fa fa-gear fs-14 text-707070"></span>
-                    <span class="font-bold fs-14 hover-color-one text-1b1c17 {{ @$subApplicationSettingActiveClass }}">{{__('Application Setting')}}</span>
-                </a>
-            </li>
-            <li>
-                <a class="align-items-center flex list-item list-item"
-                   href="{{ route('admin.setting.storage.index') }}">
-                    <span class="fa fa-gear fs-14 text-707070"></span>
-                    <span class="font-bold fs-14 hover-color-one text-1b1c17 {{ @$subStorageSettingActiveClass }}">{{ __('Storage Setting') }}</span>
-                </a>
-            </li>
-            <li>
-                <a class="align-items-center flex list-item list-item"
-                   href="{{ route('admin.setting.color-settings') }}">
-                    <span class="fa fa-gear fs-14 text-707070"></span>
-                    <span class="font-bold fs-14 hover-color-one text-1b1c17 {{ @$subColorSettingActiveClass }}">{{ __('Color & Custom Code') }}</span>
-                </a>
-            </li>
-            <li>
-                <a class="align-items-center flex list-item list-item"
-                   href="{{ route('admin.setting.logo-settings') }}">
-                    <span class="fa fa-gear fs-14 text-707070"></span>
-                    <span class="font-bold fs-14 hover-color-one text-1b1c17 {{ @$subLogoSettingActiveClass }}">{{ __('Logo Setting') }}</span>
-                </a>
-            </li>
-            <li>
-                <a class="align-items-center flex list-item list-item"
-                   href="{{ route('admin.setting.maintenance') }}">
-                    <span class="fa fa-gear fs-14 text-707070"></span>
-                    <span class="font-bold fs-14 hover-color-one text-1b1c17 {{ @$subMaintenanceModeActiveClass }}">{{ __('Maintenance Mode') }}</span>
-                </a>
-            </li>
+<div class="nav flex-column nav-pills" role="tablist" aria-orientation="vertical">
+    <a class="nav-link {{ @$subApplicationSettingActiveClass }}"
+       href="{{ route('admin.setting.application-settings') }}">
+        {{ __('App Settings') }}
+    </a>
 
-            <li>
-                <a href="{{ route('admin.setting.cache-settings') }}" class="align-items-center flex list-item list-item">
-                    <span class="fa fa-gear fs-14 text-707070"></span>
-                    <span class="font-bold fs-14 hover-color-one text-1b1c17 {{ @$subCacheActiveClass }}">{{ __('Cache Settings') }}</span>
-                </a>
-            </li>
-        </ul>
-    </div>
+    <a class="nav-link {{ @$subLogoSettingActiveClass }}"
+       href="{{ route('admin.setting.logo-settings') }}">
+        {{ __('Logo Settings') }}
+    </a>
+
+    <a class="nav-link {{ @$subColorSettingActiveClass }}"
+       href="{{ route('admin.setting.color-settings') }}">
+        {{ __('Color Settings') }}
+    </a>
+
+    <a class="nav-link {{ @$subStorageSettingActiveClass }}"
+       href="{{ route('admin.setting.storage.index') }}">
+        {{ __('Storage Settings') }}
+    </a>
+
+    <a class="nav-link {{ @$subMaintenanceModeActiveClass }}"
+       href="{{ route('admin.setting.maintenance') }}">
+        {{ __('Maintenance Mode') }}
+    </a>
+
+    <a class="nav-link {{ @$subCacheActiveClass }}"
+       href="{{ route('admin.setting.cache-settings') }}">
+        {{ __('Cache Settings') }}
+    </a>
 </div>
