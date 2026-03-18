b7745086a76ea076832f1138c524dd93d8ff1dff
ashik-zainik-lab <ashik.200607.zan@gmail.com>
2026-03-18 10:52:12 +0600

    working o cache settings

--- resources/views/admin/setting/cache-settings.blade.php
+++ resources/views/admin/setting/cache-settings.blade.php
@@ -1,56 +1,115 @@
-@extends('layouts.app')
+@extends('admin.layouts.app')
 @push('title')
 {{ $title }}
 @endpush
 @section('content')
 <div class="p-30">
-    <div class="">
-        <h4 class="fs-24 fw-500 lh-34 text-black pb-16">{{ __($title) }}</h4>
-        <div class="row">
-            <div class="col-xxl-2 col-lg-3 col-md-4 pr-0">
-                <div class="bd-c-ebedf0 bd-half bd-ra-25 bg-white h-100 p-30">
-                    @include('admin.setting.partials.general-sidebar')
-                </div>
-            </div>
-            <div class="col-xxl-10 col-lg-9 col-md-8">
+    <!-- Page title -->
+    <div class="dashboard-section-title">
+        <h2 class="title">{{ __($title) }}</h2>
+        <p>{{ __('Manage and clear different types of application cache.') }}</p>
+    </div>
 
-                <div class="bg-white bd-half bd-c-ebedf0 bd-ra-25 p-30">
-                    <div class="email-inbox__area bg-style form-horizontal__item bg-style admin-general-settings-page">
-                        <div class="item-top mb-30">
-                            <h4>{{ $title }}</h4>
-                        </div>
-                        <div class="item-top mb-30"><h2>{{ __(@$pageTitle) }}</h2></div>
-                        <div class="custom-form-group mb-3 row align-items-center">
-                            <label for="" class="col-lg-3 text-lg-right text-black">{{ __('Clear View Cache') }}</label>
-                            <div class="col-lg-9">
-                                <a href="{{ route('admin.setting.cache-update', 1) }}" class="fs-15 fw-500 lh-25 text-black py-10 px-26 bg-cdef84 bd-ra-12 hover-bg-one">{{ __('Click Here') }}</a>
+    <!-- Settings layout like html/setting.html -->
+    <div class="d-flex align-items-start settings-grid-container">
+        <!-- Left sidebar -->
+        <aside class="settings-sidebar">
+            @include('admin.setting.partials.general-sidebar')
+        </aside>
+
+        <!-- Right content -->
+        <div class="settings-forms w-100">
+            <div class="dashboard-settings-card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ __(@$pageTitle ?: $title) }}</h5>
+
+                    <div class="row g-3">
+                        <div class="col-xxl-6 col-lg-6 col-md-12">
+                            <div class="dashboard-settings-card h-100">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <h6 class="card-title mb-1">{{ __('Clear View Cache') }}</h6>
+                                        <p class="text-muted mb-3">{{ __('Remove compiled blade views.') }}</p>
+                                    </div>
+                                    <div class="text-end">
+                                        <a href="{{ route('admin.setting.cache-update', 1) }}"
+                                           class="primary_button">
+                                            {{ __('Click Here') }}
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="custom-form-group mb-3 row align-items-center">
-                            <label for="" class="col-lg-3 text-lg-right text-black">{{ __('Clear Route Cache') }} </label>
-                            <div class="col-lg-9">
-                                <a href="{{ route('admin.setting.cache-update', 2) }}" class="fs-15 fw-500 lh-25 text-black py-10 px-26 bg-cdef84 bd-ra-12 hover-bg-one">{{ __('Click Here') }}</a>
+
+                        <div class="col-xxl-6 col-lg-6 col-md-12">
+                            <div class="dashboard-settings-card h-100">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <h6 class="card-title mb-1">{{ __('Clear Route Cache') }}</h6>
+                                        <p class="text-muted mb-3">{{ __('Refresh cached routes.') }}</p>
+                                    </div>
+                                    <div class="text-end">
+                                        <a href="{{ route('admin.setting.cache-update', 2) }}"
+                                           class="primary_button">
+                                            {{ __('Click Here') }}
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="custom-form-group mb-3 row align-items-center">
-                            <label for="" class="col-lg-3 text-lg-right text-black">{{ __('Clear Config Cache') }} </label>
-                            <div class="col-lg-9">
-                                <a href="{{ route('admin.setting.cache-update', 3) }}" class="fs-15 fw-500 lh-25 text-black py-10 px-26 bg-cdef84 bd-ra-12 hover-bg-one">{{ __('Click Here') }}</a>
+
+                        <div class="col-xxl-6 col-lg-6 col-md-12">
+                            <div class="dashboard-settings-card h-100">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <h6 class="card-title mb-1">{{ __('Clear Config Cache') }}</h6>
+                                        <p class="text-muted mb-3">{{ __('Reload configuration values.') }}</p>
+                                    </div>
+                                    <div class="text-end">
+                                        <a href="{{ route('admin.setting.cache-update', 3) }}"
+                                           class="primary_button">
+                                            {{ __('Click Here') }}
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="custom-form-group mb-3 row align-items-center">
-                            <label for="" class="col-lg-3 text-lg-right text-black">{{ __('Application Clear Cache') }} </label>
-                            <div class="col-lg-9">
-                                <a href="{{ route('admin.setting.cache-update', 4) }}" class="fs-15 fw-500 lh-25 text-black py-10 px-26 bg-cdef84 bd-ra-12 hover-bg-one">{{ __('Click Here') }}</a>
+
+                        <div class="col-xxl-6 col-lg-6 col-md-12">
+                            <div class="dashboard-settings-card h-100">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <h6 class="card-title mb-1">{{ __('Application Clear Cache') }}</h6>
+                                        <p class="text-muted mb-3">{{ __('Clear application cache store.') }}</p>
+                                    </div>
+                                    <div class="text-end">
+                                        <a href="{{ route('admin.setting.cache-update', 4) }}"
+                                           class="primary_button">
+                                            {{ __('Click Here') }}
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="custom-form-group mb-3 row align-items-center">
-                            <label for="" class="col-lg-3 text-lg-right text-black">{{ __('Storage Link') }} </label>
-                            <div class="col-lg-9">
-                                <a href="{{ route('admin.setting.cache-update', 5) }}" class="fs-15 fw-500 lh-25 text-black py-10 px-26 bg-cdef84 bd-ra-12 hover-bg-one">{{ __('Click Here') }}</a>
+
+                        <div class="col-xxl-6 col-lg-6 col-md-12">
+                            <div class="dashboard-settings-card h-100">
+                                <div class="card-body d-flex flex-column justify-content-between">
+                                    <div>
+                                        <h6 class="card-title mb-1">{{ __('Storage Link') }}</h6>
+                                        <p class="text-muted mb-3">{{ __('Recreate storage symbolic link.') }}</p>
+                                    </div>
+                                    <div class="text-end">
+                                        <a href="{{ route('admin.setting.cache-update', 5) }}"
+                                           class="primary_button">
+                                            {{ __('Click Here') }}
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
