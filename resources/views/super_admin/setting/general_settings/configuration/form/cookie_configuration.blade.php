72a6ccc6a4a1069f1d55b509c40d321b88340a73
ashik-zainik-lab <ashik.200607.zan@gmail.com>
2026-03-18 15:29:14 +0600

    fixed

--- resources/views/super_admin/setting/general_settings/configuration/form/cookie_configuration.blade.php
+++ resources/views/super_admin/setting/general_settings/configuration/form/cookie_configuration.blade.php
@@ -1,31 +1,27 @@
-<div class="customers__area mb-30">
-    <div class="item-top mb-30 d-flex flex-wrap justify-content-between">
-        <h2>{{__('Cookie Configuration')}}</h2>
-        <div class="mClose">
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><img
-                    src="{{asset('assets/images/icon/delete.svg')}}" alt=""></button>
-        </div>
+<div>
+    <div class="modal-header">
+        <h2>{{ __('Cookie Configuration') }}</h2>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="{{ __('Close') }}"></button>
     </div>
+
     <form class="ajax" action="{{ route('super_admin.setting.common.settings.update') }}" method="post"
-          class="form-horizontal" data-handler="commonResponseForModal">
+        data-handler="commonResponseForModal">
         @csrf
-        <div class="row">
-            <div class="col-12 mb-4">
-                <div class="primary-form-group mt-2">
-                    <div class="primary-form-group-wrap">
-                        <label class="form-label">{{__('Cookie Consent Text')}} </label>
-                        <textarea class="primary-form-control" name="cookie_consent_text" cols="30"
-                                  rows="10">{{getOption('cookie_consent_text')}}</textarea>
+
+        <div class="modal-body">
+            <div class="row g-3">
+                <div class="col-12">
+                    <div class="dashboard-form-group">
+                        <label for="cookie_consent_text" class="form-label">{{ __('Cookie Consent Text') }}</label>
+                        <textarea class="primary-form-control" name="cookie_consent_text" id="cookie_consent_text" rows="6">{{ getOption('cookie_consent_text') }}</textarea>
                     </div>
                 </div>
-
             </div>
         </div>
-        <div class="row mb-3">
-            <div class="col-md-12 text-end">
-                <button class="fs-15 border-0 fw-500 lh-25 text-black py-10 px-26 bg-cdef84 bd-ra-12 hover-bg-one"
-                        type="submit">{{ __('Update') }}</button>
-            </div>
+
+        <div class="modal-footer form-actions">
+            <button type="button" class="primary_button cancel" data-bs-dismiss="modal">{{ __('Cancel') }}</button>
+            <button type="submit" class="primary_button">{{ __('Update') }}</button>
         </div>
     </form>
 </div>
